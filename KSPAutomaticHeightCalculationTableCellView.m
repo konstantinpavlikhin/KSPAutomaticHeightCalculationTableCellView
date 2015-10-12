3cd25331da840cb6e4beee01c1c76a2290a6a3c8
Konstantin Pavlikhin <k.pavlikhin@gmail.com>
2015-10-12 11:41:02 +0300

    Bump a copyright year.

--- KSPAutomaticHeightCalculationTableCellView.m
+++ KSPAutomaticHeightCalculationTableCellView.m
@@ -3,7 +3,7 @@
 //  KSPAutomaticHeightCalculationTableCellView
 //
 //  Created by Konstantin Pavlikhin on 13.10.14.
-//  Copyright (c) 2014 Konstantin Pavlikhin. All rights reserved.
+//  Copyright (c) 2015 Konstantin Pavlikhin. All rights reserved.
 //
 
 #import "KSPAutomaticHeightCalculationTableCellView+Private.h"
