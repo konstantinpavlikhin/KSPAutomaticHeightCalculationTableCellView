60b5cbf5dc8485bc7240652c2b279c78798a5470
Konstantin Pavlikhin <k.pavlikhin@gmail.com>
2016-09-01 13:54:33 +0300

    Observe the 'objectValue' property and update the text label.

--- Sample/Sources/AHCMessageTableCellView.m
+++ Sample/Sources/AHCMessageTableCellView.m
@@ -8,8 +8,47 @@
 
 #import "AHCMessageTableCellView+Private.h"
 
+// * * *.
+
+static void* ObjectValueKVOContext;
+
+// * * *.
+
 @implementation AHCMessageTableCellView
 
 @dynamic objectValue;
+
+#pragma mark - Cleanup
+
+- (void) dealloc
+{
+  [self removeObserver: self forKeyPath: NSStringFromSelector(@selector(objectValue)) context: &ObjectValueKVOContext];
+}
+
+#pragma mark -
+
+- (void) awakeFromNib
+{
+  [super awakeFromNib];
+
+  // * * *.
+
+  [self addObserver: self forKeyPath: NSStringFromSelector(@selector(objectValue)) options: 0 context: &ObjectValueKVOContext];
+}
+
+#pragma mark - Key-Value Observation
+
+- (void) observeValueForKeyPath: (NSString*) keyPath ofObject: (id) object change: (NSDictionary<NSString*,id>*) change context: (void*) context
+{
+  if(context == &ObjectValueKVOContext)
+  {
+    // Text.
+    self.textLabel.objectValue = self.objectValue;
+  }
+  else
+  {
+    [super observeValueForKeyPath: keyPath ofObject: object change: change context: context];
+  }
+}
 
 @end
