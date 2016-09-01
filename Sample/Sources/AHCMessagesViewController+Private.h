bf433d24d88ccbce24e43775f66f9402e2a7383f
Konstantin Pavlikhin <k.pavlikhin@gmail.com>
2016-09-01 13:28:43 +0300

    Prepare methods to be implemented in a MessagesViewController.

--- Sample/Sources/AHCMessagesViewController+Private.h
+++ Sample/Sources/AHCMessagesViewController+Private.h
@@ -10,7 +10,11 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface AHCMessagesViewController ()
+@interface AHCMessagesViewController () <NSTableViewDataSource, NSTableViewDelegate>
+
+@property(readwrite, strong, nonatomic) IBOutlet NSTableView* tableView;
+
+@property(readwrite, strong, nonatomic) IBOutlet NSTableColumn* mainColumn;
 
 @end
 
--- Sample/Sources/AHCMessagesViewController.m
+++ Sample/Sources/AHCMessagesViewController.m
@@ -12,6 +12,57 @@ NS_ASSUME_NONNULL_BEGIN
 
 @implementation AHCMessagesViewController
 
+#pragma mark -
+
+- (void) awakeFromNib
+{
+  [super awakeFromNib];
+
+  // * * *.
+
+  [self loadMessages];
+
+  [self registerCells];
+
+  // * * *.
+
+  [self.tableView reloadData];
+}
+
+#pragma mark - Private Methods
+
+- (void) loadMessages
+{
+}
+
+- (void) registerCells
+{
+}
+
+#pragma mark - NSTableViewDataSource Protocol Implementation
+
+- (NSInteger) numberOfRowsInTableView: (NSTableView*) tableView
+{
+  return 0;
+}
+
+- (nullable id) tableView: (NSTableView*) tableView objectValueForTableColumn: (nullable NSTableColumn*) tableColumn row: (NSInteger) row
+{
+  return nil;
+}
+
+#pragma mark - NSTableViewDelegate Protocol Implementation
+
+- (nullable NSView*) tableView: (NSTableView*) tableView viewForTableColumn: (nullable NSTableColumn*) tableColumn row: (NSInteger) row
+{
+  return nil;
+}
+
+- (CGFloat) tableView: (NSTableView*) tableView heightOfRow: (NSInteger) row
+{
+  return 0;
+}
+
 @end
 
 NS_ASSUME_NONNULL_END
